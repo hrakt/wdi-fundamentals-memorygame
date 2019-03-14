53235b993ea21076311f062831bda844fb637ebc
Hrak Torosian <Hrak@Hraks-MacBook-Pro.local>
2019-03-14 10:32:54 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardInPlay = [];
-var cardOne = cards[0];
-cardInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[3];
-cardInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if(cardInPlay.length === 2){
-	if (cardInPlay[0] === cardInPlay[1]) {
-		alert("You found a match");
-	}else{
-		alert("Sorry try again!");
-	}
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+	if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		  console.log("You found a match!");
+		} else {
+		  console.log("Sorry, try again.");
+	}	
 }
+}
+
+var flipCard = function(cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
