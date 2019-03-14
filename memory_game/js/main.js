0607d7301072018fe9c17412f7bb097feaa728c7
Hrak Torosian <Hrak@Hraks-MacBook-Pro.local>
2019-03-14 12:52:31 -0700

    create game llogic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,22 +23,39 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	if(cardsInPlay.length === 2){
+
+	
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-		  console.log("You found a match!");
+		  alert("You found a match!");
 		} else {
-		  console.log("Sorry, try again.");
+		  alert("Sorry, try again.");
 	}	
-}
+
 }
 
-var flipCard = function(cardID){
+var flipCard = function(){
+	var cardID = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardID].rank);
 	console.log(cards[cardID].suit);
 	console.log(cards[cardID].cardImage);
 	cardsInPlay.push(cards[cardID].rank);
+	this.setAttribute('src', cards[cardID].cardImage);
+	if(cardsInPlay.length === 2){
 	checkForMatch();
+	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+   		var cardElement = document.createElement('img');
+   		cardElement.setAttribute('src', 'images/back.png');	
+   		cardElement.setAttribute('data-id',i);
+   		cardElement.addEventListener('click', flipCard);
+   		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+
+
+};
+
+createBoard();
